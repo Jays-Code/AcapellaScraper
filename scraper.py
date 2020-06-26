af04565e1d83b8034c1a6a02db36c15572b9215d
Jays-Code <dfbfbc@gmail.com>
2020-06-26 18:01:55 -0400

    scraper now reflecting front page of yotube with query

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup as bs
 import requests
+from flask import Flask
+
+app = Flask(__name__)
 
 
 base = "https://www.youtube.com/results?search_query="
@@ -11,16 +14,22 @@ r = requests.get(base+qstring)
 page = r.text
 soup = bs(page, 'html.parser')
 
-'''
+
+
+
+
+
 vids = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
 videolist=[]
 for v in vids:
     tmp = 'https://www.youtube.com' + v['href']
     videolist.append(tmp)
-    '''
+    
 
-print(soup)
+@app.route('/')
+def listResults():
+    return(page)
 
 '''
-print(bs)
+should return soup but that gives error: return(soup)
 '''
