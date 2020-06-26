d1564147aeaff78690b0c41ea9ed3fae190a4e37
Jays-Code <dfbfbc@gmail.com>
2020-06-26 16:01:51 -0400

    scraper scans youtube page, throws error for star unicode character u2727

--- scraper.py
+++ scraper.py
@@ -11,5 +11,16 @@ r = requests.get(base+qstring)
 page = r.text
 soup = bs(page, 'html.parser')
 
-print(bs)
+'''
+vids = soup.findAll('a',attrs={'class':'yt-uix-tile-link'})
+videolist=[]
+for v in vids:
+    tmp = 'https://www.youtube.com' + v['href']
+    videolist.append(tmp)
+    '''
 
+print(soup)
+
+'''
+print(bs)
+'''
