f3e5bbcc8b98f4d13dd374f834710c13c3746635
Jays-Code <dfbfbc@gmail.com>
2020-07-06 19:26:34 -0400

    added selenium and webdriver since beautifulsoup would not load base url with query

--- scraper.py
+++ scraper.py
@@ -1,10 +1,20 @@
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
 import requests
 from flask import Flask
 import lxml
 
 
 app = Flask(__name__)
+
+chrome_path = r"C:\Users\Jay\Desktop\chromedriver_win32\chromedriver.exe"
+driver = webdriver.Chrome(chrome_path)
+
+driver.get("https://www.youtube.com/results?search_query=isolated+vocals+acapella")
+pageContent = driver.page_source
+'''
+driver.close()
+'''
 
 
 base = "https://www.youtube.com/results?search_query="
@@ -20,7 +30,7 @@ qstring = "witter.com"
 r = requests.get(base+qstring)
 
 page = r.text
-soup = bs(page, 'lxml')
+soup = bs(pageContent, 'lxml')
 
 
 
@@ -39,7 +49,7 @@ for v in vids:
 @app.route('/')
 def listResults():
     print(vids)
-    return(page)
+    return(pageContent)
 
 
     
@@ -49,4 +59,21 @@ should return soup but that gives error: return(soup)
 '''
 '''
 example url for reference: https://www.youtube.com/watch?v=eB0nUzAI7M8
+'''
+
+
+
+'''
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import os
+driver = webdriver.Firefox(executable_path="/home/rishabh/Documents/pythonProjects/webScarapping/geckodriver")
+driver.get('https://www.youtube.com/feed/trending')
+content = driver.page_source
+driver.close()
+
+soup = BeautifulSoup(content, 'html.parser')
+#Do whatever you want with it
 '''
